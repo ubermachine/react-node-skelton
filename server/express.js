81e3fae324cc3338bc6d9c2f768b9b451098e7a9
ubermachine <shivamkatoch@outlook.com>
2020-07-27 23:55:45 +0530

    basic setup done

--- server/express.js
+++ server/express.js
@@ -9,10 +9,13 @@ import compress from "compression";
 import Template from "./../template";
 import userRoutes from "./routes/user.routes";
 import authRoutes from "./routes/auth.routes";
+import devBundle from "./devBundle"; //comment out before building for production
 
 const CURRENT_WORKING_DIR = process.cwd();
 
 const app = express();
+//comment out before building for production
+devBundle.compile(app);
 app.user("/dist", express.static(path.join(CURRENT_WORKING_DIR, "dist")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
